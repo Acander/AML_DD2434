2d0ba9ff99b622e22a8295e1a5ced9844799845a
Acander <adrianz@kth.se>
2019-12-03 00:40:58 +0100

    Finished step two

--- Assignment_1/LinearRegression.py
+++ Assignment_1/LinearRegression.py
@@ -16,22 +16,41 @@ from mpl_toolkits.mplot3d import Axes3D
 #Date pairs has same indecies in x and t
 
 def main():
-    data = generateDataSet2D(1.5, 0.5)
+    numberOfDataPoints = 200
+    data = generateDataSet2D(-1.5, 0.5, numberOfDataPoints)
     x = data[0]
     t = data[1]
 
-    print("Input vector: ")
-    print(x)
-    print("\n")
-    print("Output vector: ")
-    print(t)
+    printRawData(x, t)
 
     pb.scatter(x, t)
-    pb.plot
+    pb.title("True Model Samples")
+    pb.xlabel("x")
+    pb.ylabel("t")
+    pb.show()
 
     prior = generate2DPrior()
     likelihood = generateLikelihood2D()
-    posterior = generate2DPosteriorFromSinglePoint(x, t, prior, likelihood)
+
+    n = 7
+    sampledIndices = np.random.random_integers(0, numberOfDataPoints-1, n)
+    dataPointsX = np.array([])
+    dataPointsT = np.array([])
+    for i in range(n):
+        dataPointsX = np.append(dataPointsX, x[sampledIndices[i]])
+        dataPointsT = np.append(dataPointsT, t[sampledIndices[i]])
+
+    dataPoints = [dataPointsX, dataPointsT]
+    posterior = generate2DPosteriorFromDataPoints(dataPoints, prior, likelihood)
+
+    sampledData = sampleDataPoints(5, posterior)
+    print(sampledData)
+    w0, w1 = sampledData.T
+    pb.scatter(w0, w1)
+    pb.title("Posterior Model Samples")
+    pb.xlabel("w0")
+    pb.ylabel("w1")
+    pb.show()
 
     #Prep plot for prior and posterior
     x = np.linspace(-3, 3, 500)
@@ -59,6 +78,19 @@ def main():
 
     pb.show()
 
+def sampleDataPoints(numberOfPoints, distribution):
+    mean = distribution.mean
+    covariance = distribution.cov
+
+    return np.random.multivariate_normal(mean, covariance, numberOfPoints)
+
+def printRawData(x,  t):
+    print("Input vector: ")
+    print(x)
+    print("\n")
+    print("Output vector: ")
+    print(t)
+    print("\n")
 
 def generate2DPrior():
     return multivariate_normal([0, 0], [[1, 0], [0, 1]])
@@ -66,37 +98,51 @@ def generate2DPrior():
 def generateLikelihood2D():
     return multivariate_normal([0, 0], [[0.2, 0], [0, 0.2]])
 
-def generate2DPosteriorFromSinglePoint(xList, tList, prior, likelihood):
-    x = xList[7]
-    x = np.array([1, x])
-    t = tList[7]
+def generate2DPosteriorFromDataPoints(dataPoints, prior, likelihood):
+    xList = dataPoints[0]
+    t = dataPoints[1]
+
+    x = np.array([[1, xList[0]]])
+    for i in range(xList.size - 1):
+        x = np.append(x, [[1, xList[i+1]]], axis=0)
+
+    print(x)
     tau = prior.cov
     sigma = likelihood.cov
 
-    """print(tau)
-    print(np.linalg.inv(tau))
-    print("\n")"""
+    printPriorAndLikelihoodCovariance(tau, sigma)
 
-    """print(sigma)
-    print(np.linalg.inv(sigma))
-    print("\n")"""
+    covariance = np.linalg.inv(np.dot(np.linalg.inv(sigma), np.dot(x, x)) + np.linalg.inv(tau))
+    mean = np.dot(np.dot(covariance, np.linalg.inv(sigma)), np.dot(x, t))
 
-    covariance = np.linalg.inv(np.linalg.inv(sigma) * x * x + np.linalg.inv(tau))
-    mean = np.dot(covariance*np.linalg.inv(sigma), x*t)
-
-    """print(np.dot(np.linalg.inv(sigma), x*x))"""
-
-    print(x)
-    print("\n")
-    print(t)
-    print("---------------")
-    print(mean)
-    print("---------------")
-    print(covariance)
+    printDatapoint(x, t)
+    printPosteriorParameters(mean, covariance)
     return multivariate_normal(mean, covariance)
 
-def generateDataSet2D(w0, w1):
-    x = np.random.uniform(-1, 1, 200)
+def printPriorAndLikelihoodCovariance(tau, sigma):
+    print(tau)
+    print(np.linalg.inv(tau))
+    print("\n")
+
+    print(sigma)
+    print(np.linalg.inv(sigma))
+    print("\n")
+    print("--------------------------")
+
+def printDatapoint(x, t):
+    print(x)
+    print(t)
+    print("\n")
+    print("---------------------------")
+
+def printPosteriorParameters(mean, covariance):
+    print(mean)
+    print(covariance)
+    print("\n")
+    print("---------------------------")
+
+def generateDataSet2D(w0, w1, numberOfDataPoints):
+    x = np.random.uniform(-1, 1, numberOfDataPoints)
     t = np.array([])
     for i, xi in enumerate(x):
         ti = w1 * xi + w0 + np.random.normal(0, 0.2)
