1830051f90ccc9a89f41d74f30070678c5d1edba
Acander <adrianz@kth.se>
2019-12-06 18:05:07 +0100

    Non-parametric Regression

---
+++ Assignment_1/NonParametricRegression2.py
@@ -0,0 +1,131 @@
+import pylab as pb
+import numpy as np
+from math import pi
+from scipy.spatial.distance import cdist
+from scipy.stats import multivariate_normal
+from mpl_toolkits.mplot3d import Axes3D
+
+# To sample from a multivariate Gaussian
+# f = np.random.multivariate_normal(mu,K);
+# To compute a distance matrix between two sets of vectors
+# D = cdist(x1,x2)
+# To compute the exponetial of all elements in a matri
+# E = np.exp(D)
+
+#Data sets has the the shape [x, t] where x is the input vector and t is the output vector
+#Date pairs has same indecies in x and t
+
+def main():
+    data = generateDataSet()
+    x = data[0]
+    t = data[1]
+
+    printRawData(x, t)
+    pb.scatter(x, t)
+    pb.title("True Model Samples")
+    pb.xlabel("x")
+    pb.ylabel("t")
+    pb.show()
+
+    sigma = 0.2
+    l = [0.2, 0.5, 0.9, 5]
+    for i in range(4):
+        prior = generateGPPrior(x, sigma, l[i])
+        samples = np.random.multivariate_normal(prior.mean, prior.cov, 50)
+        plotCurves(samples)
+
+def plotCurves(samples):
+    pb.plot(samples)
+    #pb.xlim(-4, 4)
+    pb.show()
+
+def sampleDataPoints(numberOfPoints, distribution):
+    mean = distribution.mean
+    covariance = distribution.cov
+
+    return np.random.multivariate_normal(mean, covariance, numberOfPoints)
+
+def printRawData(x,  t):
+    print("Input vector: ")
+    print(x)
+    print("\n")
+    print("Output vector: ")
+    print(t)
+    print("\n")
+
+def generateGPPrior(x, sigma, l):
+    gramKernel = kernel(x, sigma, l)
+    print(gramKernel)
+    print(np.zeros(len(x)))
+    return multivariate_normal(np.zeros(len(x)), gramKernel)
+
+#Squared Exponential covariance function
+def kernel(x, sigma, l):
+    gram = []
+    print(gram)
+    for j in range(len(x)):
+        Xdiff = (x - x[j])
+        exp = Xdiff*Xdiff/(l*l)
+        gramRow = sigma*sigma*np.exp(-exp)
+        gram.append(gramRow)
+
+    return np.array(gram)
+
+def generateLikelihood2D(sigma):
+    return multivariate_normal([0, 0], [[sigma, 0], [0, sigma]])
+
+def generate2DPosteriorFromDataPoints(dataPoints, prior, likelihood):
+    xList = dataPoints[0]
+    t = dataPoints[1]
+
+    x = np.array([[1, xList[0]]])
+    for i in range(xList.size - 1):
+        x = np.append(x, [[1, xList[i+1]]], axis=0)
+
+    print(x)
+    tau = prior.cov
+    sigma = likelihood.cov
+
+    printPriorAndLikelihoodCovariance(tau, sigma)
+
+    covariance = np.linalg.inv(np.dot(np.linalg.inv(sigma), np.dot(x.transpose(), x)) + np.linalg.inv(tau))
+    mean = np.dot(np.dot(covariance, np.linalg.inv(sigma)), np.dot(x.transpose(), t))
+
+    printDatapoint(x, t)
+    printPosteriorParameters(mean, covariance)
+    return multivariate_normal(mean, covariance)
+
+def printPriorAndLikelihoodCovariance(tau, sigma):
+    print(tau)
+    print(np.linalg.inv(tau))
+    print("\n")
+
+    print(sigma)
+    print(np.linalg.inv(sigma))
+    print("\n")
+    print("--------------------------")
+
+def printDatapoint(x, t):
+    print(x)
+    print(t)
+    print("\n")
+    print("---------------------------")
+
+def printPosteriorParameters(mean, covariance):
+    print(mean)
+    print(covariance)
+    print("\n")
+    print("---------------------------")
+
+def generateDataSet():
+    x = np.array([-4, -3, -2, -1, 0, 2, 3, 5])
+    t = np.array([])
+    for i, xi in enumerate(x):
+        ti = (2 + (0.5 * xi) * (0.5 * xi)) * np.sin(3 * xi) + np.random.normal(0, 0.3)
+        t = np.append(t, ti)
+
+    return [x, t]
+
+
+if __name__ == "__main__":
+    main()
